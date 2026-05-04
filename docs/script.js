542210074b9479cd479559d1529cce2d7d68caed
blaivere09 <84152809+blaivere09@users.noreply.github.com>
2026-05-04 17:35:34 +0200

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,16 +1,27 @@
 function acceptarCookies() {
-    document.getElementById('cookieBox').style.display = 'none';
-    localStorage.setItem('cookies_foodlogistic_v2', 'acceptades');
+    document.getElementById('cookieBox').style.opacity = '0';
+    setTimeout(() => {
+        document.getElementById('cookieBox').style.display = 'none';
+    }, 500);
+    localStorage.setItem('food_cookies_v3', 'accepted');
 }
 
 window.onload = function() {
-    if (localStorage.getItem('cookies_foodlogistic_v2')) {
+    if (localStorage.getItem('food_cookies_v3')) {
         document.getElementById('cookieBox').style.display = 'none';
     }
 }
 
 document.getElementById('contactForm').addEventListener('submit', function(e) {
     e.preventDefault();
-    alert("Formulari enviat correctament. Hem registrat el seu consentiment legal.");
-    this.reset();
+    const btn = this.querySelector('.btn-main');
+    btn.innerText = "Enviant dades segures...";
+    btn.style.background = "#888";
+
+    setTimeout(() => {
+        alert("Enviament reeixit. FoodLogístic S.A. ha registrat el seu consentiment i es posarà en contacte aviat.");
+        this.reset();
+        btn.innerText = "Enviar Sol·licitud d'Informació";
+        btn.style.background = "#4CAF50";
+    }, 2000);
 });
